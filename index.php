176e0897edbd9cab4a919a5562f6a281a5f16a7b
Ritik Patel <77116911+Ritikpatel963@users.noreply.github.com>
2024-03-10 13:34:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,275 @@
+<?php
+include 'header.php';
+?>
+<!-- banner-secction -->
+<div id="carouselExampleIndicators" class="carousel slide mt-2" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="image/plum-1690579_1280.jpg" class="d-block w-100" alt="..." height="400px">
+            </div>
+            <div class="carousel-item">
+                <img src="image/plums-3560078_1280.jpg" class="d-block w-100" alt="..." height="400px">
+            </div>
+            <div class="carousel-item">
+                <img src="image/plums-3560078_1280.jpg" class="d-block w-100" alt="..." height="400px">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+<!--  -->
+<!-- marquee section -->
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-12 xs-padding0">
+            <div id="blog-homepage" class="row">
+                <div class="col-sm-4" style="width:400">
+                    <div class="post">
+                        <blockquote>
+                            <h4 class="text-left">
+                                <i class="fa fa-bell black-text" aria-hidden="true"></i> Announcements
+                            </h4>
+                        </blockquote>
+                        <hr class="notice-boxHR">
+
+                        <div class="scroll-marquee">
+                            <marquee direction="up" scrollamount="3" scrolldelay="200" behavior="scroll" onmouseover="this.stop();" onmouseout="this.start();" height="300">
+                                <panel id="" style="font-weight: normal; font-size: 15px;">
+                                    <panel class="text-justify">
+                                        <ol>
+
+                                            <a href="documents/meetings/Change of Dates of AGM.pdf">
+                                                <h4>
+                                                    <li> <img src="images/newg.gif">
+                                                        <strong>Postponement of AIU's 98th Annual Meet and National Conference
+                                                            of Vice Chancellors</strong>, which was proposed to be held during
+                                                        April 04-06, 2024 is <strong>postponed to April 15-17, 2024</strong>
+                                                    </li>
+                                                </h4>
+                                            </a><a href="https://docs.google.com/forms/d/e/1FAIpQLSdnbj__lg7sDOcSR2l-bFqTZgjy26PUmB4yoZb8R4hy2-DlQQ/viewform?vc=0&amp;c=0&amp;w=1&amp;flr=0">
+                                                Registration/Travel Programme Proforma
+                                            </a>
+                                        </ol>
+                                    </panel>
+                                </panel>
+                            </marquee>
+                        </div>
+
+                        <p class="read-more">
+                            <a href="#" class="btn btn-primary btn-xs read_more">Read more</a>
+                        </p>
+                    </div>
+                </div>
+
+
+                <!-- NOTICES -->
+                <div class="col-sm-4" style="width:400">
+                    <div class="post">
+                        <blockquote>
+                            <h4 class="text-left">
+                                <i class="fa fa-file black-text" aria-hidden="true"></i> Notice
+                            </h4>
+                        </blockquote>
+                        <hr class="notice-boxHR">
+
+                        <div class="scroll-marquee">
+                            <marquee direction="up" scrollamount="3" scrolldelay="200" behavior="scroll" onmouseover="this.stop();" onmouseout="this.start();" height="300">
+                                <panel id="fvNews_ctl00_pdfnews" style="font-weight: normal; font-size: 15px;">
+                                    <panel class="text-justify">
+                                        <ol>
+                                            <a href="documents/meetings/Expression of Interest (EoI) for holding the AIU Annual General Body Meet 2025.pdf">
+                                                <h4>
+                                                    <li class="right"> <img src="images/newg.gif">
+                                                        Invitation of <strong>Expression of Interest (EoI)</strong> for AIU’s
+                                                        99th Annual General Body Meet and National Conference of Vice
+                                                        Chancellors’ for the year 2025 (for the AIU Member Universities of North
+                                                        Zone)
+                                                        <br>
+                                                    </li>
+                                                </h4>
+                                            </a>
+                                        </ol><strong>
+                                        </strong>
+                                    </panel><strong>
+                                    </strong>
+                                </panel><strong>
+                                </strong>
+                            </marquee>
+                        </div>
+                        <p class="read-more">
+                            <a href="#" class="btn btn-primary btn-xs read_more">Read more</a>
+                        </p>
+                    </div>
+                </div>
+                <style type="text/css">
+                    .Title {
+                        font-weight: bold;
+                        color: red;
+                        text-transform: uppercase;
+                        font-size: 15px;
+
+                    }
+
+                    .Details {
+                        font-weight: 400;
+                        font-style: italic;
+                        color: blue;
+                        padding-left: 5px;
+                        font-size: 15px;
+                    }
+
+                    .centerCell {
+                        text-align: center;
+                    }
+
+                    .Issue {
+                        font-weight: bold;
+                        color: GREEN;
+                        text-transform: uppercase;
+
+                    }
+                </style>
+                <!-- NEW Initiatives -->
+                <div class="col-sm-4" style="width:400">
+                    <div class="post">
+                        <blockquote>
+                            <h4 class="text-left">
+                                <i class="fa fa-book black-text" aria-hidden="true"></i>New Initiatives
+                            </h4>
+                        </blockquote>
+                        <hr class="notice-boxHR">
+                        <div class="scroll-marquee">
+                            <marquee direction="up" scrollamount="3" scrolldelay="200" behavior="scroll" onmouseover="this.stop();" onmouseout="this.start();" height="300">
+                                <panel id="" style="font-weight: normal;">
+                                    <panel class="text-justify">
+                                        <ol>
+                                            <h4 class="Title">
+                                                <li style="color:RED;">
+                                                    <!-- <img src="images/newg.gif" />  -->
+                                                    <a class="Issue" href="documents/index/AIU Nodal Officers.xlsx">
+                                                        List of AIU’s Nodal Officers along with Universities allotted to them,
+                                                        to attend AIU related activities for better coordination
+                                                    </a>
+                                                </li>
+                                            </h4>
+                                            <a class="Issue" href="documents/youthaffairs/Letter reg Kuch Artistic Karona.pdf">
+                                            </a>
+                                            <h4 class="Title"><a class="Issue" href="documents/youthaffairs/Letter reg Kuch Artistic Karona.pdf"></a>
+                                                <li style="color:RED;"><a class="Issue" href="documents/youthaffairs/Letter reg Kuch Artistic Karona.pdf">
+                                                        <!-- <img src="images/newg.gif" />  -->
+                                                    </a><a href="documents/youthaffairs/2020/AIU_Letter_reg_Kuch_Artistic_Karo-Na.pdf"><strong>
+                                                            Results of AIU Initiative to Unite India through “Kuch Artistic
+                                                            Karo-na”
+                                                        </strong></a></li>
+                                            </h4><a href="documents/youthaffairs/2020/AIU_Letter_reg_Kuch_Artistic_Karo-Na.pdf"><strong>
+                                                </strong></a><strong><a class="Issue" href="documents/youthaffairs/2020/Rules_Online_Contest_2nd_Round.pdf">
+                                                </a>
+                                                <h5 class="Title" style="color:BLUE;"><a class="Issue" href="documents/youthaffairs/2020/Rules_Online_Contest_2nd_Round.pdf">
+                                                        RULES &amp; REGULATIONS for the 2nd &amp; Final Round of UNITE INDIA -an
+                                                        online initiative by AIU (KUCH ARTISTIC KARONA)
+                                                    </a>
+                                                    <a class="Issue" href="documents/youthaffairs/Letter reg Kuch Artistic Karona.pdf">
+                                                        <h4 class="Title">
+                                                            <li style="color:RED;">
+                                                                <!--  <img src="images/newg.gif" /> -->
+                                                                Regarding New Online Initiative by AIU-INDIA UNITE-Kuch Artistic
+                                                                Karo-na(Share your video showing your talent through Dance,
+                                                                Acting, Nazam/ Gazals, Poetry By <strong>May 10, 2020</strong>).
+                                                            </li>
+                                                        </h4>
+                                                    </a><a class="Issue" href="documents/youthaffairs/Poster.pdf">
+                                                    </a>
+                                                </h5>
+                                            </strong>
+                                        </ol><strong>
+                                        </strong>
+                                    </panel><strong>
+                                    </strong>
+                                </panel><strong>
+                                </strong>
+                            </marquee>
+                        </div>
+
+                        <p class="read-more">
+                            <a href="#" class="btn btn-primary btn-xs read_more">Read more</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--  -->
+<!-- President photo and secretory photo  -->
+<div class="container" style="margin-top: 40px;">
+    <div class="row">
+        <div class="col-sm-6" style="display: flex; justify-content: center;">
+            <div class="card" style="width: 15rem;  border:none">
+                <img class="card-img-top" src="image/Prof G.D. Sharma.jpg" alt="Card image cap" style="  border-radius: 50%; height: 238px;">
+                <div class="card-body" style="text-align: center;">
+                    <h5 class="card-title"><strong>President, IMPA</strong></h5>
+                    <p class="card-text">Prof G.D. Sharma</p>
+                    <a href="#" class="btn btn-primary">View Profile</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6" style="display: flex; justify-content: center;">
+            <div class="card" style="width: 15rem;  border:none">
+                <img class="card-img-top" src="image/Dr Pankaj Mittal.jpg" alt="Card image cap" style="  border-radius: 50%;">
+                <div class="card-body" style="text-align: center;">
+                    <h5 class="card-title"><strong>President, IMPA</strong></h5>
+                    <p class="card-text">Prof G.D. Sharma</p>
+                    <a href="#" class="btn btn-primary">View Profile</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- login section  -->
+
+<div class="boxSection">
+    <div class="row innerSection">
+        <div class="col-md-6">
+            <div class="box box2">
+                <a href="login.php">
+                    <img src="image/register.png" alt="Register/Login">
+                    <p><span>Register / Login</span></p>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="box box3">
+                <a href="/Home/ContactUs">
+                    <img src="image/contact.png" alt="Contact">
+                    <p><span>Contact Us</span></p>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--  -->
+
+<script>
+        // JavaScript to make the carousel slide automatically
+        var myCarousel = document.getElementById('carouselExampleIndicators');
+        var carousel = new bootstrap.Carousel(myCarousel, {
+            interval: 1000, // Set the interval (in milliseconds) between slides
+            wrap: true // Set to true to enable continuous sliding
+        });
+    </script>
+<?php
+include 'footer.php';
+?>
