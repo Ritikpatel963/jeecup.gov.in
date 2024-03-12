da87707790e3473a1c14d0172b1146470ac106cc
Ritik Patel <77116911+Ritikpatel963@users.noreply.github.com>
2024-03-12 09:26:26 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,77 @@
+ <!-- footer -->
+
+ <style>
+     a {
+         color: #007bff;
+         text-decoration: none;
+         background-color: transparent;
+     }
+ </style>
+
+ <div class="footer-top">
+
+     <div class="copyright">
+         <div class="text-center my-2">
+             <a href="https://www.facebook.com/" target="_blank"><img src="image/f.png" width="30"></a>
+             <a href="https://twitter.com/" target="_blank"><img src="image/x.png" width="30" class="mx-2"></a>
+             <a href="https://youtube.com/" target="_blank"><img src="image/yt.png" width="30"></a>
+         </div>
+         <div class="container">
+         <div style="font-size: 15px; color:black;"> This site is designed, developed &amp; hosted by National Informatics Centre, Ministry of Electronics &amp; IT (MeitY), Government of India and Content owned by Department of Administrative Reforms &amp; Public Grievances.
+         </div>
+         </div>
+
+     </div>
+ </div>
+
+ <footer id="mainFooter">
+     
+     <div class="footer-bottom">
+         <div class="container">
+             <div class="row">
+                 <div class="col-xs-12 text-center">
+
+                     <div class="copyRights">
+                         <div class="copyRightsText">
+                             <p class="mr-none">Content Owned and Managed by Joint Entrance Examination Council
+                                 (Polytechnic), Uttar Pradesh
+                             </p>
+                             <p class="mr-bottom10"></p>
+                             <p>Last Updated: <strong>Mar 01, 2024</strong></p>
+                         </div>
+
+                         <div class="copyRightsLogos">
+                             <a href="#"><img src="image/s3waaslogo.svg" class="swaas" alt="Secure, Scalable and Sugamya Website as a Service"></a>
+                             <a href="#"><img src="image/Nic_logo.svg" class="nic" alt="National Informatics Centre"></a>
+                             <a href="#"><img src="image/Digital-India-white.svg" alt="Digital India"></a>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+     <div class="footer-top">
+         <div class="container">
+             <div class="col-xs-12 text-center">
+                 <div class="footerMenu">
+                     <ul id="menu-footer-en" class="menu">
+                         <li id="menu-item-6880" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-6880"><a href="copyright-policy/index.html">Copyright Policy</a></li>
+                         <li id="menu-item-6879" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-6879"><a href="privacy-policy/index.html">Privacy Policy</a></li>
+                         <li id="menu-item-6878" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-6878"><a href="hyperlink-policy/index.html">Hyperlink Policy</a></li>
+                         <li id="menu-item-6877" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-6877"><a href="terms-and-conditions/index.html">Terms and Conditions</a></li>
+                         <li id="menu-item-735" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-735"><a href="help/index.html">Help</a></li>
+                         <li id="menu-item-736" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-736"><a href="contact-us/index.html">Contact Us</a></li>
+                     </ul>
+                 </div>
+             </div>
+         </div>
+     </div>
+
+ </footer>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-RfcJzfGrR9s2Wos2FZnSkVGCZv3YOyClaxkZLlz7IMZVSqmbKbSdsPfhYqla8CCE" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-RRGJklTNhOkLJfv7NPVFd5JQo9Z/LFzKbBbJUSozda86E//rODk57E8jo8f3xemz" crossorigin="anonymous"></script>
+ </body>
+
+ </html>
