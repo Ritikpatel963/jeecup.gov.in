7433ea3b8dcbd8a687449449ee0cf9b24d6abc3f
Ritik Patel <77116911+Ritikpatel963@users.noreply.github.com>
2024-03-12 09:25:46 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,223 @@
+<html>
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
+    <meta http-equiv="cache-control" content="no-cache, must-revalidate, max-age=0">
+<meta http-equiv="expires" content="0">
+<meta http-equiv="pragma" content="no-cache">
+<link rel="stylesheet" href="styles.css?v=2">
+<?php
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+?>
+    <style>
+        .btn-info {
+            width: 100%;
+        }
+    </style>
+</head>
+<div class="header">
+    <div id="topBar">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="push-right d-none d-md-block" aria-label="Primary">
+                        <div id="accessibility">
+                            <ul id="accessibilityMenu" class="clearfix">
+                                <li>
+                                    <a href="#SkipContent" class="skipContent" aria-label="Skip to main content" title="Skip to main content">
+                                        <span class="m-hide">Skip to main content</span>
+                                        <span class="icon-skip-to-main m-show"></span>
+                                    </a>
+                                </li>
+                                <li class="languageCont" aria-label="Change Language">
+                                    <div class="dropdown">
+                                    <a href="" class="language link-selected" aria-label="English - Selected" title="English - Selected" role="button" data-toggle="dropdown">English</a>
+                                            
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">Hindi</a></li>
+
+                                        </ul>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="push-left">
+                        <div class="govBranding">
+                            <ul>
+                                <li>
+                                    <a href="/">Welcome to IMPA India</a>
+                                </li>
+                                <li>
+                                    <a href="#">lbspcup@gmail.com | 08057728847</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- header_wrapper -->
+<!--header middle start-->
+<div class="header-wrapper" style="margin-top: 15px;">
+    <div class="container">
+        <div class="row ecounse-logo-wpr">
+            <div class="col-md-3">
+                <div class="logo main-logo">
+                    <a href="index.php" class="site_logo" rel="home" title="Go to home">
+                        <img class="emblem state-emb" id="logo" src="image/header_wrapper1.png" alt="Government of Uttar Pradesh">
+                        <div class="logo_text">
+                            <strong lang="hi"></strong>
+                            <h1 class="h1-logo"><span class="hide"> / </span></h1>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="logo dept-logo">
+                    <div class="site_logo">
+                        <div class="logo_text">
+                            <strong lang="hi">संयुक्त प्रवेश परीक्षा (पॉलिटेक्निक), उत्तर प्रदेश</strong>
+                            <div class="h1-logo logo-en-text heading1">Joint Entrance Examination Council
+                                (Polytechnic), Uttar Pradesh</div>
+                            <p class="dept-subtitle">Examination, Admission and eCounselling Services for
+                                Session 2024</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 header-rightlogo">
+                <div class="header-right hidden-xs push-right">
+                    <a aria-label="Joint Entrance Examination Council - External site that opens in a new window" href="#" target="_blank" title="Joint Entrance Examination Council">
+                        <img class="sw-logo" src="image/header_wrapper2.png" alt="Joint Entrance Examination Council">
+                    </a>
+                </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<!--header middle end-->
+
+
+<!--  -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="container" style="margin-top: 20px; display: flex; justify-content: center;">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="gap: 10px;">
+                    <li class="nav-item">
+                        <button class="btn btn-info btn-lg" style="background-color: #002366;"><a href="index.php">
+                                <font color="white"><i class="fa-solid fa-house"></i></i> Home</font>
+                            </a>
+                        </button>
+                    </li>
+                    <li class="nav-item">
+                        <div class="dropdown">
+                            <button class="btn btn-info btn-lg dropdown-toggle" style="background-color: #002366;" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false"><!-- <span class="glyphicon">&#xe086;</span>  --> About IPMA</button>
+                            <div class="dropdown-content">
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <li>
+                                        <a class="dropdown-item" href="">Genesis</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="">Vision &amp; Mission</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="">Objectives</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="">Regulations</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="">Members</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="">Governance</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="">President</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="">Secretary General</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="">Annual Reports</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <div class="dropdown">
+                            <button class="btn btn-info btn-lg " type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false" style="background-color: #002366;"> <i class="fa fa-sitemap" aria-hidden="true"></i> Organisation</button>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="dropdown">
+                            <button class="btn btn-info btn-lg" style="background-color: #002366;"><i class="fa fa-line-chart" aria-hidden="true"></i> FAQ </button>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="dropdown">
+                            <button class="btn btn-info btn-lg" style="background-color: #002366;"><i class="fa fa-line-chart" aria-hidden="true"></i> RTI</button>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="dropdown">
+                            <button class="btn btn-info btn-lg dropdown-toggle" style="background-color: #002366;"><a href="">
+                                <i style="color: white" class="fa-solid fa-user"></i>
+                                    <font color="white">Membership </font>
+                                </a></button>
+                            <div class="dropdown-content">
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <li>
+                                        <a class="dropdown-item" href="history.php">Apply For Membership</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="history.php">Benefits of membership</a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                    </li>
+                     <li class="nav-item">
+                            <button class="btn btn-info btn-lg" style="background-color: #002366;"><a href="">
+                                    <font color="white"><i class="fa fa-futbol-o" aria-hidden="true"></i>Contact Us</font>
+                                </a></button>
+                    </li>
+                    <!--<li class="nav-item">
+                            <div class="dropdown">
+                                <button class="btn btn-info btn-lg" style="background-color: #002366;"> Equivalence</button>
+                            </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="dropdown">
+                            <button class="btn btn-info btn-lg" style="background-color: #002366;"> New Initiatives</button>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="dropdown">
+                            <button type="button" class="btn btn-info btn-lg">AIU Anveshan 2023</button>
+                        </div>
+                    </li>  -->
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
