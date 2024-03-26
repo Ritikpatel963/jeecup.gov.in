418a965a0d9c90c8d2c38144113bd2297b264e97
Ritik Patel <77116911+Ritikpatel963@users.noreply.github.com>
2024-03-26 14:24:26 +0530

    Add files via upload

---
+++ About-us.php
@@ -0,0 +1,53 @@
+<?php
+include 'header.php';
+?>
+<style>
+    #page-content  h1, .head-banner h1 {
+        text-align: center;
+        color: white;
+    }
+
+    .head-banner {
+        background-color: #0F7B53;
+        padding-top: 100;
+        padding-bottom: 100;
+    }
+</style>
+<div class="head-banner">
+    <h1>About Us</h1>
+</div>
+<div class="container" id="page-content">
+    <div class="card mt-5" style="margin-bottom: 10%;">
+        <div class="card-body">
+            <p class="card-text">
+                <h3 style="text-align:left; font-family: initial; line-height: inherit;">
+                <strong>Introduction to the indian Paramedical Association:</strong>
+
+<p>Welcome to the indian Paramedical Association, a beacon of support and advocacy for the unsung heroes of healthcare – the paramedical staff. Committed to their rights and upliftment, we stand as a unified voice dedicated to ensuring their recognition, well-being, and professional growth.</p>
+
+<p><strong>Who We Are:</strong></br>
+The indian Paramedical Association is a collective of passionate individuals deeply interested for the upliftment in the healthcare sector. From dotors to physiotherapist to paramedics to educationists to businessmen  to advocates to company secretary to common man , we encompass a diverse array of professionals and individuals. United by a common goal, we strive to foster a supportive community and empower our members to excel in their vital roles within the healthcare ecosystem.</p>
+
+
+            </h3>
+            <h3 style="text-align: left;; font-family: initial; line-height: inherit;"><strong>Our Objective:</strong>
+</br>At the heart of our mission lies a steadfast commitment to championing the rights and uplifting the status of paramedical staff. Our objectives are multifaceted, aiming to</h3>
+            <h3 style="text-align: left;; font-family: initial; line-height: inherit;"><strong>Advocate for Recognition:</strong></br> We advocate for the acknowledgment of paramedical professionals as integral pillars of healthcare delivery. Through education and awareness initiatives, we seek to highlight the invaluable contributions they make to patient care and public health.
+
+                <p><strong> Promote Professional Development: </strong></br>We are dedicated to nurturing the professional growth of paramedical staff. By facilitating training programs, workshops, and access to resources, we empower them to continually enhance their skills, knowledge, and expertise.</p>
+
+                <p><strong>Ensure Workplace Rights: </strong><br> We vehemently uphold the rights of paramedical professionals in the workplace. From fair remuneration to safe working conditions, we strive to create an environment where their contributions are respected, valued, and fairly compensated.</p>
+
+                <p><strong>Facilitate Networking and Collaboration:</strong> <br> Through networking events, conferences, and forums, we provide opportunities for paramedical professionals to connect, collaborate, and share best practices. By fostering a sense of camaraderie and solidarity, we strengthen the collective voice of our members.</p>
+
+                <p><strong>Community Engagement:</strong> <br> Beyond the confines of healthcare institutions, we engage with communities to raise awareness about the role and importance of paramedical professionals. Through outreach programs and initiatives, we seek to bridge gaps in healthcare access and promote health literacy.</p>
+
+                <p>As we embark on this journey towards greater recognition and empowerment, we invite paramedical professionals from all corners to join us in our endeavors. Together, let us champion the cause of those who tirelessly dedicate themselves to the well-being of others. Welcome to the indian Paramedical Association – where passion meets purpose, and advocacy transforms</p>
+            </h3>
+        </div>
+    </div>
+</div>
+
+<?php
+include 'footer.php';
+?>
