9879712fea88ea46dd20c4968444be143abfd29b
Ritik Patel <77116911+Ritikpatel963@users.noreply.github.com>
2024-03-26 14:24:49 +0530

    Add files via upload

---
+++ Empowerment_and_Professional_Growth.php
@@ -0,0 +1,31 @@
+<?php
+include 'header.php';
+?>
+<style>
+    h1 {
+        text-align: center;
+        padding-top: 150;
+        padding-bottom: 150;
+        color: white;
+    }
+    .head-banner{
+        background-color: #20325a;
+    }
+</style>
+<div class="head-banner">
+    <h1>Empowerment and Professional Growth</h1>
+</div>
+<div class="container">
+    <div class="card mt-5" style="margin-bottom: 10%;">
+        <div class="card-body">
+            <p class="card-text">
+            <h3 style="text-align: center;">We aspire to empower paramedical professionals with the tools, resources, and opportunities they need to thrive in their careers. Our vision includes comprehensive professional development programs, mentorship opportunities, and avenues for career advancement, enabling them to reach their full potential and excel in their respective fields.
+</h3>
+
+        </div>
+    </div>
+</div>
+
+<?php
+include 'footer.php';
+?>
