5262c28a9d2c201bea39962c11a31791061fbd76
Ritik Patel <77116911+Ritikpatel963@users.noreply.github.com>
2024-03-29 12:17:17 +0530

    Add files via upload

---
+++ Ethical_Practice_and_Social_Responsibility.php
@@ -0,0 +1,31 @@
+<?php
+include 'header.php';
+?>
+<style>
+    h1 {
+        text-align: center;
+        padding-top: 150;
+        padding-bottom: 150;
+        color: white;
+    }
+    .head-banner{
+        background-color: #20325a;
+    }
+</style>
+<div class="head-banner">
+    <h1>Ethical Practice and Social Responsibility</h1>
+</div>
+<div class="container">
+    <div class="card mt-5" style="margin-bottom: 10%;">
+        <div class="card-body">
+            <p class="card-text">
+            <h3 style="text-align: center;">We envision a future where paramedical professionals uphold the highest standards of ethical practice and social responsibility. Our vision includes promoting ethical conduct, cultural sensitivity, and social accountability in all aspects of their work, ensuring equitable access to healthcare services for all individuals and communities.</br>In pursuit of this vision, the Paramedical Association remains steadfast in its commitment to championing the rights, well-being, and professional advancement of paramedical professionals, both now and in the years to come. Together, let us strive to create a brighter, healthier future for all.
+</h3>
+
+        </div>
+    </div>
+</div>
+
+<?php
+include 'footer.php';
+?>
